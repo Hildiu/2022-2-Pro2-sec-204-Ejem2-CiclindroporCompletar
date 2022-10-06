305a3ce7c6bdaee8f52167d9c02e6729cc48d003
Hildiu <31174932+Hildiu@users.noreply.github.com>
2022-10-06 18:54:13 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*--------------------------------------------------
+ * Dato de Entrada: radio, altura (double)
+ * Dato de Salida : asupercieCilindrica,
+ *                  areaLateral
+ *                  areaTotal
+ *                  Volumen (double)
+ ---------------------------------------------------*/
+
+
+#include <iostream>
+
+int main()
+{
+
+    return 0;
+}
